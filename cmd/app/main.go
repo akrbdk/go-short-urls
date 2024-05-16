ffa89233afe4ee3a26609091b09af350b47abb72
alexander.karabudka <sanyakarabudka@gmail.com>
2024-05-16 19:16:42 +0200

    base steps

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("Short urls makes the world better!")
+	//TODO init config
+	//TODO init logger
+	//TODO init storage
+	//TODO init router
+	//TODO run app
 }
